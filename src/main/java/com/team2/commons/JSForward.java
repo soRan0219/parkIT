e74b53f427bc4018d6a66ef6a484779265507192
cocochichi <psi8090@naver.com>
2023-05-03 16:49:58 +0900

    수정과 삭제는 진행중입니다..ㅎ

---
+++ src/main/java/com/team2/commons/JSForward.java
@@ -0,0 +1,49 @@
+package com.team2.commons;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+public class JSForward {
+	// 자바스크립트 사용해서 페이지 이동
+	
+	// 1. alert 메세지 출력, 페이지 뒤로가기
+	public static void alertAndBack(HttpServletResponse response
+													, String msg){ // 메서드 만들기
+		// response 자바에서 사용 X, 파라미터 값을 줘서 사용하기
+
+		
+		try {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();		
+			out.println("<script>");
+			out.println("alert('"+msg+"!');");
+			out.println("history.back();");
+			out.println("</script>");
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	// 2. alert 메세지 출력, 원하는 위치로 이동
+	public static void alertAndMove(HttpServletResponse response, 
+											String msg, String location) {
+		
+		try {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('"+msg+"');");
+			out.println("location.href='"+location+"';");
+			out.println("</script>");
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}		
+		
+	}
+
+}
---
+++ src/main/webapp/member/memberDelete.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+	// 로그인 세션 제어		
+// 	String id = (String)session.getAttribute("id");
+// 	if(id == null ){
+// 		response.sendRedirect("main.jsp");
+// 	}
+	%>
+	
+	<c:if test="${empty id }"> <!-- sessionScope.id -->
+	 	<c:redirect url="./MemberLogin.park"/>
+	</c:if>
+
+	<h1>memberDelete.jsp</h1>
+	<h2>회원정보 삭제(탈퇴)(TEAM2)</h2>
+
+	<fieldset>
+		<form action="./MemberDeleteAction.park" method="post">
+			<input type="hidden" name="id" value="${id }">
+			비밀번호 : <input type="password" name="pw">
+			<input type="submit" value="탈퇴하기">
+		</form>
+	</fieldset>
+	
+
+	
+	
+	
+	
+	
+</body>
+</html>
