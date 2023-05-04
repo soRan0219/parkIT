9df1080a69aa9bbd86855bed488692a44e79c464
soRan0219 <thfks0219@gmail.com>
2023-05-04 11:37:48 +0900

    2023.05.04 - 주차장코드,날짜,입출차시간으로 예약 가능한 자리 조회와 예약 상세페이지에서 재조회(ajax)

---
+++ src/main/java/com/team2/reservation/action/AvailableAction.java
@@ -0,0 +1,101 @@
+package com.team2.reservation.action;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import com.team2.admin.db.ResDTO;
+import com.team2.commons.Action;
+import com.team2.commons.ActionForward;
+import com.team2.parkingdetail.db.PDetailDTO;
+import com.team2.reservation.db.ResDAO;
+
+public class AvailableAction implements Action {
+	
+	public Time stringToTime(String time) {
+		Time parsedTime = null;
+		try {
+			String sTime = time.split(" ")[4];
+			SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss", Locale.ENGLISH);
+			Date parsedDate = dateFormat.parse(sTime);
+			parsedTime = new Time(parsedDate.getTime());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return parsedTime;
+	} //stringToTime()
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("M: AvailableAction_execute()");
+		
+		request.setCharacterEncoding("utf-8");
+		
+		//희망 예약 날짜 
+		String dateString = request.getParameter("date");
+//		System.out.println(dateString);
+		
+		SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'Z",Locale.ENGLISH);
+		Date parsedDate = dateFormat.parse(dateString);
+		java.sql.Date resDate = new java.sql.Date(parsedDate.getTime());
+//		System.out.println(resDate);
+		
+		//희망 입,출차 시간 
+		String fromTime = request.getParameter("fromTime");
+		String toTime = request.getParameter("toTime");
+		Time parkInTime = stringToTime(fromTime);
+		Time parkOutTime = stringToTime(toTime);
+//		System.out.println(parkInTime);
+//		System.out.println(parkOutTime);
+		
+		//주차장 잔여 자리 정보 조회
+		String parkingCode = request.getParameter("parkingCode");
+//		System.out.println("parkingCode = " + parkingCode);
+		
+		ResDTO rDto = new ResDTO();
+		rDto.setParkingCode(parkingCode);
+		rDto.setParkInTime(parkInTime);
+		rDto.setParkOutTime(parkOutTime);
+		rDto.setResDate(resDate);
+		
+		ResDAO dao = new ResDAO();
+		
+		int price = dao.getPrice(parkInTime, parkOutTime);
+		System.out.println("price: " + price);
+		List<PDetailDTO> aList = dao.getAvailable(rDto);
+		
+		JSONArray jArr = new JSONArray();
+		
+		Iterator it = aList.iterator();
+		if(it.hasNext()) {
+			for(int i=0; i<aList.size(); i++) {
+				JSONObject jobj = new JSONObject();
+				jobj.put("parkingCode", aList.get(i).getParkingCode());
+				jobj.put("parkingPosition", aList.get(i).getParkingPosition());
+				jArr.add(jobj);
+			}
+		}
+		JSONObject jobj = new JSONObject();
+		jobj.put("price", price);
+		jArr.add(jobj);
+		
+		System.out.println(jArr.size());
+		
+		response.setContentType("application/x-json; charset=utf-8");
+		response.getWriter().print(jArr);
+		
+		return null;
+	} //execute()
+
+} //AvailableAction
---
+++ src/main/java/com/team2/reservation/action/ReservationAction.java
@@ -0,0 +1,89 @@
+package com.team2.reservation.action;
+
+import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.team2.admin.db.ParkingDTO;
+import com.team2.admin.db.ResDTO;
+import com.team2.commons.Action;
+import com.team2.commons.ActionForward;
+import com.team2.parkingdetail.db.PDetailDTO;
+import com.team2.reservation.db.ResDAO;
+
+public class ReservationAction implements Action {
+	
+	public Time stringToTime(String time) {
+		Time parsedTime = null;
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("H:mm", Locale.ENGLISH);
+			java.util.Date parsedDate = dateFormat.parse(time);
+			parsedTime = new Time(parsedDate.getTime());
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		
+		return parsedTime;
+	} //stringToTime()
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		System.out.println("M: ReservationAction_execute()");
+		
+		request.setCharacterEncoding("utf-8");
+		
+		//희망 예약 날짜 
+		String dateString = request.getParameter("selectedDate");
+		Date resDate = Date.valueOf(LocalDate.parse(dateString));
+		request.setAttribute("resDate", resDate);
+		
+		//희망 입,출차 시간 
+		String fromTime = request.getParameter("fromTime");
+		String toTime = request.getParameter("toTime");
+		Time parkInTime = stringToTime(fromTime);
+		Time parkOutTime = stringToTime(toTime);
+		
+		request.setAttribute("parkInTime", parkInTime);
+		request.setAttribute("parkOutTime", parkOutTime);
+		
+		//주차장 정보 조회
+		String parkingCode = request.getParameter("parking");
+		System.out.println("parkingCode = " + parkingCode);
+		
+		ResDAO dao = new ResDAO();
+		ParkingDTO pDto = dao.getParking(parkingCode);
+		request.setAttribute("pDto", pDto);
+		
+		//해당 주차장 모든 자리
+		List<PDetailDTO> allList = dao.getAllParkingDetail(parkingCode);
+		request.setAttribute("allList", allList);
+		
+		//예약 가능 주차 자리 조회
+		ResDTO rDto = new ResDTO();
+		rDto.setParkingCode(parkingCode);
+		rDto.setParkInTime(parkInTime);
+		rDto.setParkOutTime(parkOutTime);
+		rDto.setResDate(resDate);
+		
+		List<PDetailDTO> available = dao.getAvailable(rDto);
+		request.setAttribute("available", available);
+		
+		//결제 예상금액
+		int price = dao.getPrice(parkInTime, parkOutTime);
+		request.setAttribute("price", price);
+		
+		ActionForward forward = new ActionForward();
+		forward.setPath("./reservation/reservation.jsp");
+		forward.setRedirect(false);
+		
+		return forward;
+	} //execute()
+
+} //ReservationAction
